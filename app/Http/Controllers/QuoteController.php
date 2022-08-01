63c19b2e3681ec9ed519d6c35db8561a2f2d7dc5
isok7071 <isok7071@gmail.com>
2022-08-01 18:52:09 +0300

    heroku

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -35,7 +35,7 @@ class QuoteController extends Controller
             }
         }
         if ($quote->save() && $quote_tags->save()) {
-            return response()->json(['status' => true, 'message' => 'Quote added successfully']);
+            return response()->json('ok','200', ['Access-Control-Allow-Origin'=>'*']);
         } else {
             return response()->json(['status' => false, 'message' => 'Quote doesnt added']);
         }
